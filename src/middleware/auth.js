32d101e2b1acc15d069958d3589ab44b17497eaf
Nabaz esmail <nabazesmail567@gmail.com>
2023-08-10 17:13:42 +0300

    almost done with the google auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,39 +1,43 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-const { User } = require('../models'); // Import your User model
-const { generateToken } = require('../utils/tokenUtils'); // Import your token generation function
+const {
+  User
+} = require('../models'); // Import your User model
+const {
+  generateToken,
+  generateTemporaryPassword
+} = require('../utils/tokenUtils'); // Import your token generation function and temporary password generation function
 
 passport.use(
-  new GoogleStrategy(
-    {
+  new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret:  process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 'http://localhost:3000/auth/google/callback',
       passReqToCallback: true,
     },
-     async function (request, accessToken, refreshToken, profile, done) {
+    async function (request, accessToken, refreshToken, profile, done) {
       try {
         // Check if the user already exists in the database based on email
-        let user = await User.findOne({ where: { email: profile.email } });
+        let user = await User.findOne({
+          where: {
+            email: profile.email
+          }
+        });
 
         if (!user) {
-          // If user doesn't exist, create a new user record
-          const temporaryPassword = "temporary123"; // Use a static temporary password
+          // If user doesn't exist, generate a unique temporary password
+          const temporaryPassword = generateTemporaryPassword(); // Generate a unique temporary password
+
+          console.log(temporaryPassword);
+          // Create a new user record
           user = await User.create({
             email: profile.email,
             username: profile.displayName,
-            password: temporaryPassword, // Set the temporary password
+            password: temporaryPassword,
+            profile_image: profile.picture,
             // You can add other properties here if needed
           });
-
-          // // Perform password validation
-          // const validationResult = await user.validatePassword();
-          // if (validationResult !== true) {
-          //   // Handle validation error, e.g., delete the user record and return an error response
-          //   await user.destroy();
-          //   return done(validationResult, null);
-          // }
         }
 
         // Generate JWT token
@@ -42,15 +46,17 @@ passport.use(
           email: user.email,
           username: user.username,
         });
-        console.log(token)
-        return done(null, { user, token });
+
+        return done(null, {
+          user,
+          token
+        });
       } catch (error) {
         return done(error, null);
       }
     }
   )
 );
-
 
 passport.serializeUser(function (user, done) {
   done(null, user);
--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -6,4 +6,28 @@ function generateToken(payload) {
   return token;
 }
 
-module.exports = { generateToken };
+
+// Function to generate a random string of specified length
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
+}
+
+// Generate a unique temporary password within the required length range
+function generateTemporaryPassword() {
+  const passwordLength = Math.floor(Math.random() * (15 - 8 + 1)) + 8; // Generate a random length between 8 and 15
+  const randomPart = generateRandomString(passwordLength - 5); // Subtracting 5 for timestamp part
+  const timestampPart = Date.now().toString(36).slice(-5); // Use the last 5 characters of timestamp
+  return `${randomPart}-${timestampPart}`;
+}
+
+module.exports = {
+  generateToken , generateTemporaryPassword,
+};
+
+
